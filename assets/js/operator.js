5b17e550087171c9d3d3472f121ac8cf822823e7
IqbalSaliAlparissy12 <iqbalsalialparissy@gmail.com>
2023-08-11 20:33:20 +0700

    #7 update untuk future/pokemon

---
+++ assets/js/operator.js
@@ -0,0 +1,21 @@
+let age = ""
+
+console.log("masukan nilai = ", age)
+if(age === 18){
+    console.log("sudah mencukupi 18 tahun")
+}else{
+    console.log("belum cukup dewasa")
+}
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
