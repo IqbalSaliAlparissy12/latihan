4d65c0744d7932825c1cab8b36020fd251c68e15
IqbalSaliAlparissy12 <iqbalsalialparissy@gmail.com>
2023-08-15 21:52:18 +0700

    #11 Update untuk belajar kelas future/class

---
+++ assets/js/class.js
@@ -0,0 +1,46 @@
+// class Student{
+//     constructor(nama, kelas, gender){
+//         this.nama = nama
+//         this.kelas= kelas
+//         this.gender = gender
+//     }
+// }
+
+// const student_func_1 = new Student("Iqbal", "FWA", "L")
+// const student_func_2 = new Student("Erina", "FWA", "P")
+// const student_func_3 = new Student("Rahmat", "FWA", "L")
+// const student_func_4 = new Student("Tia", "FWA", "P")
+
+// console.log(student_func_1);
+// console.log(student_func_2);
+// console.log(student_func_3);
+// console.log(student_func_4);
+
+class student{
+    #nama
+    #kelas
+    #gender
+
+    constructor(nama, kelas, gender){
+        this.nama = nama 
+        this.kelas = kelas
+        this.gender = gender
+    }
+    helloSiswa(){
+        console.log(`Hallo ${this.nama}, Selamat Malam`)
+    }
+}
+const student_func_1 = new Student("Iqbal", "FWA", "L")
+const student_func_2 = new Student("Erina", "FWA", "P")
+const student_func_3 = new Student("Rahmat", "FWA", "L")
+const student_func_4 = new Student("Tia", "FWA", "P")
+
+console.log(student_func_1);
+console.log(student_func_2);
+console.log(student_func_3);
+console.log(student_func_4);
+
+student_func_1.helloSiswa()
+student_func_2.helloSiswa()
+student_func_3.helloSiswa()
+student_func_4.helloSiswa()
